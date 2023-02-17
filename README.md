af2fd3ebd8538f4653f91660bee6f65cfacb7571
gosborcz <malgorzata.m.borczyk@gmail.com>
2023-02-17 13:16:49 +0000

    edger analysis

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 In the scripts folder this repo contains 4 .R with RNAseq data analysis tutorials:
 
-1. annotations.R[link] - this files serves as an introduction to R and explores various annotations of genes and transcripts (e.g. transcript types, ensembl IDs)
-2. edger_analysis.R[link] - this file is an example of RNAseq data analysis with the edgeR library
+1. [annotations.R](scripts/annotations.R) - this files serves as an introduction to R and explores various annotations of genes and transcripts (e.g. transcript types, ensembl IDs)
+2. [edger_analysis.R](scripts/edger_analysis.R) - this file is an example of RNAseq data analysis with the edgeR library
 3. anova_analysis.R[link] - this file contains an example analysis of RNAseq with ANOVA
 4. visualisation.R[link] - this file plots heatmaps
 
---
+++ scripts/edger_analysis.R
@@ -0,0 +1,69 @@
+#installing EdgeR package - run this only once
+
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("edgeR")
+
+#script starts here, remember to add your comments
+
+require(edgeR)
+require(GO.db)
+require(tidyverse)
+require(org.Hs.eg.db)
+
+
+#Task 1 - read the /rnaseq-tutorials/data/with_ref/salmon.merged.gene_counts.tsv file into R
+
+rawdata <- #put your answer here
+ 
+y <- DGEList(counts=rawdata[,3:10],
+             genes=rawdata[,1:2])
+
+rownames(y$counts) <- rownames(y$genes) <- y$genes$gene_name
+
+keep <- filterByExpr(y)
+
+table(keep)
+y <- y[keep, , keep.lib.sizes=FALSE]
+y <- calcNormFactors(y)
+
+plotMDS(y)
+
+ctrls <- c("H27","H45", "H91", "H74")
+ruptured <- c("H2", "H47", "H56", "H87")
+
+y$samples$group <- as.factor(c("rup",
+                               "ctrl",
+                               "ctrl",
+                               "rup",
+                               "rup",
+                               "ctrl",
+                               "rup",
+                               "ctrl"))
+
+y$samples
+
+y <- estimateDisp(y)
+et <- exactTest(y, pair=c("ctrl","rup"))
+et_results <- topTags(et, n=Inf)
+et_results <- et_results$table %>% tibble()
+
+summary(decideTests(et))
+plotMD(et)
+abline(h=c(-1, 1), col="blue")
+
+#Task 2 - annotate the results with transcript types from the gtf file
+
+# Task 3 - use enrichR (manually or via R) to look for overrepresented pathways in the results
+
+# Task 4 - make a new script edger_analysis_de_novo.R where you will analyse
+# the de novo assembly with edger
+
+
+# Task 5* - downolad data from this publication: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7691544/
+# raw counts are here: http://149.156.177.112/projects/ifpan-kinga-dieta/cuffnorm/genes.count_table
+# sample info here: http://149.156.177.112/projects/ifpan-kinga-dieta/cuffnorm/samples.table
+# and here: http://149.156.177.112/projects/ifpan-kinga-dieta/analysis/samples-info.csv
+#  and run your own two-factor analysis in a new file
+
